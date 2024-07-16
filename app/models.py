d601b32f202b7badc237e381c748108984f0a7f5
newLisa2024 <loskutovaelena50@gmail.com>
2024-07-16 21:28:56 +0300

    add all hosting

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import login_manager
 from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), index=True, unique=True)
     password = db.Column(db.String(200), nullable=False)
--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,7 @@ def index():
     return render_template('index.html')
 
 @app.route('/register', methods=['GET', 'POST'])
-def registration():
+def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = RegistrationForm()
@@ -20,9 +20,9 @@ def registration():
         user = User(username=form.username.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash('Вы успешно зарегистрировались!')
+        flash('Вы успешно зарегистрировались!', 'success')
         return redirect(url_for('login'))
-    return render_template('register.html')
+    return render_template('register.html', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -35,7 +35,7 @@ def login():
             login_user(user)
             return redirect(url_for('index'))
         else:
-            flash('Неверно введены данные аккаунта. Попробуйте еще раз!')
+            flash('Неверно введены данные аккаунта. Попробуйте еще раз!', 'danger')
     return render_template('login.html', form=form)
 
 @app.route('/logout')
